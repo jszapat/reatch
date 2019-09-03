f2a5bc140657b94a82db1f799e2e9debb3906e08
Joseph Zapata <josephzapata@Josephs-MBP.fios-router.home>
2019-09-03 19:12:19 -0400

    l1

---
+++ app.js
@@ -0,0 +1,17 @@
+//lesson 1
+
+
+import React from 'react';
+import BookList from './components/BookList';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
