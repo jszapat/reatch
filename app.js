79c46cd0f19ea9bef6ad8067bc95714f896f51f6
Joseph Zapata <josephzapata@Josephs-MBP.fios-router.home>
2019-09-03 19:57:54 -0400

    worked lesson 6

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//lesson 5
-
+//lesson 6
 
 import React from 'react';
 import BookList from './components/BookList';
 import Navbar from './components/Navbar';
 import ThemeContextProvider from './contexts/ThemeContext';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <ThemeContextProvider>
         <Navbar />
         <BookList />
+        <ThemeToggle />
       </ThemeContextProvider>
     </div>
   );
