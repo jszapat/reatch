7091bfa512fa7b007d49388a895d918fba8f9800
Joseph Zapata <josephzapata.student@careerdevs.com>
2019-09-04 18:39:14 -0400

    lx

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
-//lesson 8
+//lesson x
 
-import React from 'react';
-import BookList from './components/BookList';
-import Navbar from './components/Navbar';
-import ThemeContextProvider from './contexts/ThemeContext';
-import ThemeToggle from './components/ThemeToggle';
-import AuthContextProvider from './contexts/AuthContext';
-
-function App() {
-  return (
-    <div className="App">
-      <ThemeContextProvider>
-        <AuthContextProvider>
-          <Navbar />
-          <BookList />
-          <ThemeToggle />
-        </AuthContextProvider>
-        
-      </ThemeContextProvider>
-    </div>
-  );
-}
-
-export default App;
 
 
